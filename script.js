fd6f45ca20cb66cbe7508db29f9a52b3aee87196
Nguyễn Thanh Cảnh <thanhcanh203c1@gmail.com>
2022-06-11 10:00:31 +0700

    fix some bug, change first page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} ////////////////////
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } ////////////////////
 //// AUDIO CODE ////
 ////////////////////
 let audioContext;
@@ -8,10 +8,10 @@ let microphone, meter;
 const requestAudioAccess = () => {
   if (navigator.mediaDevices) {
     navigator.mediaDevices.getUserMedia({ audio: true }).
-    then(stream => setAudioStream(stream)).
-    catch(err => alert('Hãy cho phép tui sử dụng micro'));
+      then(stream => setAudioStream(stream)).
+      catch(err => alert('Hãy cho phép tui sử dụng micro'));
   } else
-  alert('Trình duyệt này không được');
+    alert('Trình duyệt này không được');
 };
 
 // Set up to record volume
@@ -30,7 +30,7 @@ const setAudioStream = stream => {
 
 // Check if is blowing
 let lowpass = 0;
-const ALPHA = 0.5,THRESHOLD = 0.09;
+const ALPHA = 0.5, THRESHOLD = 0.09;
 const isBlowing = () => {
   lowpass = ALPHA * meter.volume + (1.0 - ALPHA) * lowpass;
   return lowpass > THRESHOLD;
@@ -47,16 +47,17 @@ const cw = canvas.width = 800;
 const ch = canvas.height = 400;
 
 const particles = [];
-const MAX_PART_COUNT = 30;
+const MAX_PART_COUNT = 25;
 
-const REIGNITE_RATE = 2; // rate at which flame will recover
+const REIGNITE_RATE = 1; // rate at which flame will recover
 const MAX_PART_DOWNTIME = 15; // max limit at which smothered flame will recover
 
 const rand = (min, max) => min + Math.random() * (max - min);
 
 // Fire Particle
 class FlameParticle {
-  constructor(x = cw / 2, y = ch / 2) {_defineProperty(this, "update",
+  constructor(x = cw / 2, y = ch / 2) {
+    _defineProperty(this, "update",
 
 
 
@@ -68,38 +69,44 @@ class FlameParticle {
 
 
 
-    () => {
-      if (this.curLife <= 90) {
-        this.radius -= Math.min(this.radius, 0.25);
-        this.curAlpha -= 0.005;
-      }
+      () => {
+        if (this.curLife <= 90) {
+          this.radius -= Math.min(this.radius, 0.25);
+          this.curAlpha -= 0.005;
+        }
 
-      if (microphone && isBlowing())
-      this.x += rand(-meter.volume, meter.volume) * 50;
+        if (microphone && isBlowing())
+          this.x += rand(-meter.volume, meter.volume) * 50;
 
-      this.curLife -= this.speed.y;
-      this.y -= this.speed.y;
-      this.draw();
-    });_defineProperty(this, "draw",
+        this.curLife -= this.speed.y;
+        this.y -= this.speed.y;
+        this.draw();
+      }); _defineProperty(this, "draw",
 
-    () => {
-      ctx.beginPath();
-      ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-      ctx.fillStyle = `rgba(254, 252, 207, ${this.curAlpha})`;
-      ctx.fill();
-      ctx.closePath();
-    });this.radius = 15;this.speed = { x: rand(-0.5, 0.5), y: 2.5 };this.life = rand(50, 100);this.alpha = 0.5;this.x = x;this.y = y;this.curAlpha = this.alpha;this.curLife = this.life;}}
+        () => {
+          ctx.beginPath();
+          ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
+          ctx.fillStyle = `rgba(255, 183, 77, ${this.curAlpha})`;
+          ctx.fill();
+          ctx.closePath();
+        }); this.radius = 15; this.speed = { x: rand(-0.5, 0.5), y: 2.5 }; this.life = rand(50, 100); this.alpha = 0.5; this.x = x; this.y = y; this.curAlpha = this.alpha; this.curLife = this.life;
+  }
+}
 
 
 // Flame Base
-class FlameBase {constructor() {_defineProperty(this, "update",
-    this.draw = () => {
-      ctx.beginPath();
-      ctx.arc(cw / 2, ch / 2, 14, Math.PI * 2, false);
-      ctx.fillStyle = 'rgba(185, 125, 45, 0.4)';
-      ctx.fill();
-      ctx.closePath();
-    });}}
+class FlameBase {
+  constructor() {
+    _defineProperty(this, "update",
+      this.draw = () => {
+        ctx.beginPath();
+        ctx.arc(cw / 2, ch / 2, 14, Math.PI * 2, false);
+        ctx.fillStyle = 'rgba(255,167,38,0.4)';
+        ctx.fill();
+        ctx.closePath();
+      });
+  }
+}
 
 
 ////////////////////
@@ -122,14 +129,16 @@ const animate = () => {
 
   // Smother flame if user is blowing
   if (microphone && isBlowing())
-  if (particleCount > -MAX_PART_DOWNTIME) particleCount -= 1;
+    if (particleCount > -MAX_PART_DOWNTIME) particleCount -= 1;
   if (particleCount == -15) {
     var app = document.getElementById('app');
     var mess = document.getElementById('mess');
+    var body = document.getElementsByTagName('body');
+    body[0].style.backgroundColor = '#E57373';
     app.classList.add('hide');
     mess.classList.remove('hide');
     audio.play();
-  } 
+  }
 
   // draw flame
   updateParticles();
@@ -140,12 +149,12 @@ const animate = () => {
 
 // Initial particle generation
 for (let i = 0; i < MAX_PART_COUNT; i++)
-particles.push(new FlameParticle());
+  particles.push(new FlameParticle());
 
 // Interval to recover flames if smothered
 setInterval(() => {
   if (particleCount < MAX_PART_COUNT)
-  particleCount += REIGNITE_RATE;
+    particleCount += REIGNITE_RATE;
 }, 200);
 
 
